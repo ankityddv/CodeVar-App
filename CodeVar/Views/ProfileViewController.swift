515270e59dfd5077dda2ed83bfb4b217a42315ef
Ankit Yadav <yadavankit840@gmail.com>
2019-10-17 14:50:17 +0530

    Timeline and profile constraints updated.

--- CodeVar/Views/ProfileViewController.swift
+++ CodeVar/Views/ProfileViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        profileImageView.layer.cornerRadius = 20
+        profileImageView.clipsToBounds = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
          self.navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.barTintColor = UIColor(red:0.07, green:0.11, blue:0.14, alpha:1.0)
+        navigationController?.navigationBar.topItem?.title = "Profile"
+        //navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
     }
     
 
--- CodeVar/Views/TimelineViewController.swift
+++ CodeVar/Views/TimelineViewController.swift
@@ -22,19 +22,20 @@ let month = calander.component(.month, from: date)
 let eventTime = "\(hour):\(minute)"
 let eventDate = "\(day):\(month)"
 
-let day1 = "\(16):\(10)" // 15 Oct
+let day1 = "\(17):\(10)" // 15 Oct
+let day2 = "\(18):\(10)"
 
-let time1 = "\(19):\(00)"  // 07PM
-let time2 = "\(21):\(00)"  // 09PM
-let time3 = "\(00):\(00)"  // 12AM
-let time4 = "\(02):\(00)"  // 02AM
-let time5 = "\(06):\(00)"  // 06AM
-let time6 = "\(09):\(00)"  // 09AM
-let time7 = "\(10):\(00)"  // 10AM
-let time8 = "\(13):\(00)"  // 01PM
-let time9 = "\(15):\(00)"  // 03PM
-let time10 = "\(18):\(00)" // 06PM
-let time11 = "\(20):\(00)" // 08PM
+let pm7 = "\(19):\(00)"  // 07PM
+let pm9 = "\(21):\(00)"  // 09PM
+let am12 = "\(00):\(00)"  // 12AM
+let am2 = "\(02):\(00)"  // 02AM
+let am6 = "\(06):\(00)"  // 06AM
+let am9 = "\(09):\(00)"  // 09AM
+let am10 = "\(10):\(00)"  // 10AM
+let pm1 = "\(13):\(00)"  // 01PM
+let pm3 = "\(15):\(00)"  // 03PM
+let pm6 = "\(18):\(00)" // 06PM
+let pm8 = "\(20):\(00)" // 08PM
 
 
 //MARK:-
@@ -46,6 +47,23 @@ class TimelineViewController: UIViewController {
     var arr = ["Profile","About Us","Policy"]
     var iconArr = ["profile","about","policy"]
 
+    
+    //MARK:- TIMER
+    var time = 0
+    //timer
+    var timer = Timer()
+    func timerstart(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimelineViewController.action), userInfo: nil, repeats: true)
+        //
+        //(TimeInterval: 1,target: self, Selector: #selector(TimelineViewController.action), userInfo: nil, repeats: true)
+    }
+    @objc func action()
+    {
+        time += 1
+        timerLabel.text = String(time)
+    }
+    //MARK:- LABEL DECLARATION
+    
     @IBOutlet weak var progressView: MBCircularProgressBarView!
     @IBOutlet weak var currentLabel: UILabel!
     @IBOutlet weak var next1Label: UILabel!
@@ -55,6 +73,7 @@ class TimelineViewController: UIViewController {
     @IBOutlet weak var time2Label: UILabel!
     @IBOutlet weak var time3Label: UILabel!
     @IBOutlet weak var profileBttn: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,6 +82,7 @@ class TimelineViewController: UIViewController {
         profileBttn.clipsToBounds = true
         self.progressView.value = 0
         self.progressView.maxValue = CGFloat(MAXTIME)
+        timerstart()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -80,53 +100,53 @@ class TimelineViewController: UIViewController {
 
     @objc func updateProgress(){
            
-           if eventTime >= time1 && eventTime < time2  && eventDate == day1 { // 7pm to 9pm
+           if eventTime >= pm7 && eventTime < pm9  && eventDate == day1 { // 7pm to 9pm
                self.progressView.value = 8
                self.progressView.progressColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0) //yellow
                self.progressView.progressStrokeColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0)
            }
-           if eventTime > time2 && eventTime <= time3  && eventDate == day1 { // 9pm to 12am
+           if eventTime >= pm9 && eventTime < am12  && eventDate == day1 { // 9pm to 12am
                self.progressView.value = 20
                self.progressView.progressColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0) // skyblue
                self.progressView.progressStrokeColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0)
 
            }
-           if eventTime > time3 && eventTime <= time4  && eventDate == day1 {  // 12am to 2am
+           if eventTime >= am12 && eventTime < am2  && eventDate == day1 {  // 12am to 2am
                self.progressView.value = 28
                self.progressView.progressColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0) //yellow
                self.progressView.progressStrokeColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0)
            }
-           if eventTime >= time4 && eventTime < time5  && eventDate == day1 {  // 2am to 6am
+           if eventTime >= am2 && eventTime < am6  && eventDate == day1 {  // 2am to 6am
                self.progressView.value = 44
                self.progressView.progressColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0) //skyblue
                self.progressView.progressStrokeColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0)
            }
-           if eventTime >= time5 && eventTime < time6  && eventDate == day1 {  // 6am to 9am
+           if eventTime >= am6 && eventTime < am9  && eventDate == day1 {  // 6am to 9am
                self.progressView.value = 56
                self.progressView.progressColor = UIColor(red:0.64, green:0.00, blue:0.84, alpha:1.0) //purple
                self.progressView.progressStrokeColor = UIColor(red:0.64, green:0.00, blue:0.84, alpha:1.0)
            }
-           if eventTime >= time6 && eventTime < time7  && eventDate == day1 {  // 9am to 10am
+           if eventTime >= am9 && eventTime < am10  && eventDate == day1 {  // 9am to 10am
                self.progressView.value = 60
                self.progressView.progressColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0) //yellow
                self.progressView.progressStrokeColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0)
            }
-           if eventTime >= time7 && eventTime < time8  && eventDate == day1 {  // 10am to 1pm
+           if eventTime >= am10 && eventTime < pm1  && eventDate == day1 {  // 10am to 1pm
                self.progressView.value = 72
                self.progressView.progressColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0) //skyblue
                self.progressView.progressStrokeColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0)
            }
-           if eventTime >= time8 && eventTime < time9  && eventDate == day1 {  // 1pm to 3pm
+           if eventTime >= pm1 && eventTime < pm3  && eventDate == day1 {  // 1pm to 3pm
                self.progressView.value = 80
                self.progressView.progressColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0) // yellow
                self.progressView.progressStrokeColor = UIColor(red:0.90, green:0.67, blue:0.01, alpha:1.0)
            }
-           if eventTime >= time9 && eventTime < time10  && eventDate == day1 {  // 3pm to 6pm
+           if eventTime >= pm3 && eventTime < pm6  && eventDate == day1 {  // 3pm to 6pm
                self.progressView.value = 92
                self.progressView.progressColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0) // skyblue
                self.progressView.progressStrokeColor = UIColor(red:0.01, green:0.75, blue:0.91, alpha:1.0)
            }
-           if eventTime >= time1 && eventTime < time11  && eventDate == day1 {  // 7pm to 8pm
+           if eventTime >= pm7 && eventTime < pm8  && eventDate == day2 {  // 7pm to 8pm
                self.progressView.value = 100
                self.progressView.progressColor = UIColor(red:0.64, green:0.00, blue:0.84, alpha:1.0) // purple
                self.progressView.progressStrokeColor = UIColor(red:0.64, green:0.00, blue:0.84, alpha:1.0)
@@ -137,16 +157,16 @@ class TimelineViewController: UIViewController {
 
        @objc func updateLabels(){
            
-           if eventTime >= time1 && eventTime < time2 && eventDate == day1 {  // 7pm to 9pm
-               currentLabel.text = "Participant arrival & kit distribution"
-               next1Label.text = "Participant arrival & kit distribution"
+           if eventTime >= pm7 && eventTime < pm9  && eventDate == day1 {  // 7pm to 9pm
+               currentLabel.text = "Arrival"
+               next1Label.text = "Arrival"
                next2Label.text = "Round1"
                next3Label.text = "Pizza"
                time1Label.text = "07:00 PM"
                time2Label.text = "09:00 PM"
                time3Label.text = "12:00 AM"
            }
-           if eventTime >= time2 && eventTime < time3 && eventDate == day1 {  // 9pm to 12am
+           if eventTime >= pm9 && eventTime < am12 && eventDate == day1 {  // 9pm to 12am
                currentLabel.text = "Round 1"
                next1Label.text = "Round 1"
                next2Label.text = "Pizza"
@@ -155,36 +175,34 @@ class TimelineViewController: UIViewController {
                time2Label.text = "12:00 AM"
                time3Label.text = "02:00 AM"
            }
-           if eventTime >= time3 && eventTime < time4  && eventDate == day1 {  // 12am to 2am
+           if eventTime >= am12 && eventTime < am2  && eventDate == day1 {  // 12am to 2am
                currentLabel.text = "Pizza"
                next1Label.text = "Pizza"
                next2Label.text = "Round 2"
-               next3Label.text = "Fun event & giveaway"
+               next3Label.text = "Giveaway"
                time1Label.text = "12:00 AM"
                time2Label.text = "02:00 AM"
                time3Label.text = "06:00 AM"
-               
            }
-           if eventTime >= time4 && eventTime < time5 && eventDate == day1 {  // 2am to 6am
+           if eventTime >= am2 && eventTime < am6 && eventDate == day1 {  // 2am to 6am
                currentLabel.text = "Round 2"
                next1Label.text = "Round 2"
-               next2Label.text = "Fun event & giveaway"
+               next2Label.text = "Giveaway"
                next3Label.text = "Round 3"
                time1Label.text = "02:00 AM"
                time2Label.text = "06:00 AM"
                time3Label.text = "09:00 AM"
-               
            }
-           if eventTime >= time5 && eventTime <= time6 && eventDate == day1 {  // 6am to 9am
-               currentLabel.text = "Fun event & giveaway"
-               next1Label.text = "Fun event & giveaway"
+           if eventTime >= am6 && eventTime < am9 && eventDate == day1 {  // 6am to 9am
+               currentLabel.text = "Giveaway"
+               next1Label.text = "Giveaway"
                next2Label.text = "Breakfast"
                next3Label.text = "Round 3"
                time1Label.text = "06:00 AM"
                time2Label.text = "09:00 AM"
                time3Label.text = "10:00 AM"
            }
-           if eventTime >= time6 && eventTime < time7  && eventDate == day1 {  // 9am to 10am
+           if eventTime >= am9 && eventTime < am10  && eventDate == day1 {  // 9am to 10am
                currentLabel.text = "Breakfast"
                next1Label.text = "Breakfast"
                next2Label.text = "Round 3"
@@ -193,7 +211,7 @@ class TimelineViewController: UIViewController {
                time2Label.text = "10:00 AM"
                time3Label.text = "01:00 pM"
            }
-           if eventTime >= time7 && eventTime < time8  && eventDate == day1 {  // 10am to 1pm
+           if eventTime >= am10 && eventTime < pm1  && eventDate == day1 {  // 10am to 1pm
                currentLabel.text = "Round 3"
                next1Label.text = "Round 3"
                next2Label.text = "Lunch"
@@ -202,7 +220,7 @@ class TimelineViewController: UIViewController {
                time2Label.text = "01:00 PM"
                time3Label.text = "03:00 PM"
            }
-           if eventTime >= time8 && eventTime < time9  && eventDate == day1 { // 1pm to 3pm
+           if eventTime >= pm1 && eventTime < pm3  && eventDate == day1 { // 1pm to 3pm
                currentLabel.text = "Lunch"
                next1Label.text = "Lunch"
                next2Label.text = "Round 4"
@@ -211,7 +229,7 @@ class TimelineViewController: UIViewController {
                time2Label.text = "03:00 PM"
                time3Label.text = "07:00 PM"
            }
-           if eventTime >= time9 && eventTime < time10 && eventDate == day1 { // 3pm to 6pm
+           if eventTime >= pm3 && eventTime < pm6 && eventDate == day1 { // 3pm to 6pm
                currentLabel.text = "Round 4"
                next1Label.text = "Round 4"
                next2Label.text = "Closing ceremony"
@@ -220,7 +238,7 @@ class TimelineViewController: UIViewController {
                time2Label.text = "07:00 PM"
                time3Label.text = ""
            }
-           if eventTime >= time1 && eventTime < time11 && eventDate == day1 { // 7pm to 8pm
+           if eventTime >= pm7 && eventTime < pm8 && eventDate == day2 { // 7pm to 8pm
                currentLabel.text = "All the best"
                next1Label.text = "Closing ceremony"
                next2Label.text = ""
@@ -231,17 +249,6 @@ class TimelineViewController: UIViewController {
                //currentEventstaticLabel.text = ""
                //roundendinStaticLabel.text = "Closing ceremony"
            }
-           //if eventTime >= "\(19):\(35)" && eventTime <= "\(19):\(40)"  && eventDate == day1 {
-             //  currentLabel.text = "Free Hour"
-             //  next1Label.text = ""
-               //next2Label.text = "Ceremony"
-             //  next3Label.text = ""
-             //  time1Label.text = ""
-             //  time2Label.text = "07:58 PM"
-             //  time3Label.text = ""
-               //currentEventstaticLabel.text = ""
-               //roundendinStaticLabel.text = "Closing ceremony"
-           //}
        }
 
 }
